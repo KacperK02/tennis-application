2bb1844987d0dbdabb4bd12c9f71ae0937ca9d77
Kacper Krzesinski <k.krzesinski02@gmail.com>
2024-02-09 11:55:51 +0100

    Created templates

--- src/main/java/com/application/tennisApplication/TennisApplication.java
+++ src/main/java/com/application/tennisApplication/TennisApplication.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.io.IOException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TennisApplication implements CommandLineRunner{
 
 	@Autowired
