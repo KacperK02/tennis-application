8d4dbd31479dc349cc31561410ee52997d40c1f7
Kacper Krzesinski <k.krzesinski02@gmail.com>
2024-02-09 14:11:41 +0100

    Added register template

--- src/main/java/com/application/tennisApplication/controller/IndexController.java
+++ src/main/java/com/application/tennisApplication/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
 }
